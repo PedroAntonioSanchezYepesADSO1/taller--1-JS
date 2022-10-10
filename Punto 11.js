5b20599817075b935059bbb276167a4974c55ec6
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 21:26:44 -0500

    Punto 11

---
+++ Punto 11.js
@@ -0,0 +1,7 @@
+// 11. Escribir un programa que calcule el volumen de un elipsoide
+// V = (4/3) * PI * a * b *c
+let A = Number(prompt('Ingrese el valor del termino A'))
+let B = Number(prompt('Ingrese el valor del termino B'))
+let C = Number(prompt('Ingrese el valor del termino C'))
+let formula = ((4/3)*Math.PI*A*B*C) 
+console.log(`El resultado de (4/3)*(${Math.PI})*(${A})*(${B})*(${C}) es ${formula}`);
