f387cdd04d2d395deaae968b6f7ae69af143dffd
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 21:25:07 -0500

    Punto 4

---
+++ Punto 4.js
@@ -0,0 +1,6 @@
+addEventListener(`DOMContentLoaded`,()=>{
+    let base1=number(prompt(`ingrese el numero`))
+    let altura2=number(prompt(`ingrese el numero`))
+    let multi=((base1*altura2)/2) 
+    console.log(`EL AREA DEL TRIANGULO ES :${multi}`);
+})
