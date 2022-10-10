5585321efb8091baf914dfae1134133620fab501
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 21:28:47 -0500

    Punto 20

--- Punto 2.js
+++
@@ -1,7 +0,0 @@
-addEventListener(`DOMContentLoaded`,()=>{
-    let lado1=Number(prompt(`ingrese el numero:`)) 
-    let lado2=Number(prompt(`ingrese el numero:`))
-    let multi=lado1*lado2 
-
-    console.log(`el area del trianguloes:${multi}`);
-})
---
+++ Punto 20.js
@@ -0,0 +1,11 @@
+    // 21. Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución,
+    // por favor utilizar funciones matemáticas de Python para ello
+    // d = rc((x2-x1)^2 + (y2-y1)^2)
+
+    let rc = Number(prompt(`Ingrese el valor de rc`));
+    let x1 = Number(prompt(`Ingrese el valor de X1`))
+    let x2 = Number(prompt(`Ingrese el valor de x2`))
+    let y1 = Number(prompt(`Ingrese el valor de y1`))
+    let y2 = Number(prompt(`Ingrese el valor de y1`))
+    let d =  rc*((x2-x1)^2 + (y2-y1)^2)
+    console.log(`El resultado es ${d}`); 
