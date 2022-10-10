4ba53eef884e12074fc2abd91c248b69be7aab4d
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 21:24:59 -0500

    Punto 3

---
+++ Punto 3.js
@@ -0,0 +1,10 @@
+addEventListener(`DOMContentLoaded`,()=>{
+    let num1=12
+    let num2=15
+    let num3=99
+    let num4=20
+    // #PROCESO U OPERACIONES
+    let media=(num1+num2+num3+num4)/4  
+    // # SALIDA O ENTRADA DE REESULTADOS
+    console.log(`la media es :${media}`);
+})
