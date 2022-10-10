bcf45bbec009ff157dbebaa34e32fa7865a5a105
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 21:27:04 -0500

    Punto 14

---
+++ Punto 14.js
@@ -0,0 +1,6 @@
+ // 14. Una farmacia aplica al precio de los remedios el 10% de descuento, hacer un programa que
+    // ingresando el costo de los medicamentos calcules el descuento y el precio final.
+
+    let precio = Number(prompt(`Ingrese el valor del medicamento`))
+    let desc = precio-(precio*0.10)
+    console.log(`El valor del medicamento con el descuento del 10% es de ${desc}`);
