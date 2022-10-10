def32e0f584117c35c5d29f850eb89a99b9d06e4
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 21:25:22 -0500

    Punto 6

---
+++ Punto 6.js
@@ -0,0 +1,16 @@
+addEventListener(`DOMContentLoaded`,()=>{
+    // # DEFINIR VARIABLES NUEMRIAS
+    // # DATOS DE ENTRADA
+    // ## OPERACIONES BASICAS:
+    
+    // lets numbers
+    let mets=Number(2*1000)
+    let segs=Number(60*5)
+    let tiempo=Number(5)
+    // procesos let ms
+    let ms=mets/segs
+    let v=mets/tiempo
+    // #salida o entrada de resultados
+    console.log(`metros en segundos ${ms}`);
+    console.log(`velocidad ${v}`);
+})
