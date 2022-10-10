b6855af33920d2244a1eb5a2f515e86e7b84b34a
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 21:25:32 -0500

    Punto 7

---
+++ Punto 7.js
@@ -0,0 +1,14 @@
+addEventListener(`DOMContentLoaded`,()=>{
+
+    //     # DEFINIR VARIABLES NUEMRIAS
+    // # DATOS DE ENTRADA
+    // ## OPERACIONES BASICAS: * / **
+    let div=Number (4/3)
+    // ${match.PI}
+    let pi=Number (3.14)
+    let radio=Number (3)
+    // caculo div*pi*radio)**3)
+    let res=((div*pi*radio)**3)
+    // #salida o entrada de resultado
+    console.log(`el volumen de un esfera es:${res}`);
+    })
