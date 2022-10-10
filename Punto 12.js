f5dbf1b7d344ddc34d681b30e9833c90f8ada342
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 21:26:52 -0500

    Punto 12

---
+++ Punto 12.js
@@ -0,0 +1,6 @@
+    // 12. Programa que muestre el pago de una llamada telefónica sabiendo que cada minuto cuesta $355
+    // pesos y un IVA de 20%.
+    let valorMin = 355+(355*0.20)
+    let canMin = Number(prompt(`Ingrese la cantidad de minutos gastados`)) 
+    let formula = valorMin * canMin
+    console.log(`El valor a pagar por los minutos consumindos es ${formula}`);
