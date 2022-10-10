4b42973755b472de255e922e37989c43ba147d86
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 21:27:37 -0500

    Punto 18

---
+++ Punto 18.js
@@ -0,0 +1,6 @@
+    // 18. Calcular el sueldo de un empleado dados comodatos de entrada: el nombre, hrs. De trabajo y el pago
+    // en hora. Pagohora=15300
+    let nombre = (prompt(`Ingrese su nombre`));
+    let hrs = prompt(`Ingrese las horas de trabajo realizadas`)
+    let sueldo = 15300*hrs
+    console.log(`Señor ${nombre} su sueldo por ${hrs} horas de trabajo es de ${sueldo}`);
