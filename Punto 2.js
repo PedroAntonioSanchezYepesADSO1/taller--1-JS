b290ae6e12bf5d363f14ec70f694b29ca6eab008
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 21:24:44 -0500

    Punto 2

---
+++ Punto 2.js
@@ -0,0 +1,7 @@
+addEventListener(`DOMContentLoaded`,()=>{
+    let lado1=Number(prompt(`ingrese el numero:`)) 
+    let lado2=Number(prompt(`ingrese el numero:`))
+    let multi=lado1*lado2 
+
+    console.log(`el area del trianguloes:${multi}`);
+})
