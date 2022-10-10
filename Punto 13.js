8e8125f25cb0b86ba34bb92a8fc89ee82d6286d6
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 21:26:59 -0500

    Punto 13

---
+++ Punto 13.js
@@ -0,0 +1,5 @@
+    // 13. Realice un algoritmo que a partir de proporcionarle la velocidad de un automóvil expresada en
+    // kilómetros por hora, proporcione la velocidad en metros por segundos.
+    let km = Number(prompt(`Ingrese los kilometros que desea convertir a ms`))
+    let conversion = km*0.2778
+    console.log(`el valor en kilomtros sobre segundos es de ${conversion}`);
